277853cb32b527a7ffb75f7feb22c98eaf64849a
nessica98 <poksin.w@gmail.com>
2020-10-27 00:56:28 +0700

    27/10 save

--- src/app/visualize-modal/visualize-modal.component.ts
+++ src/app/visualize-modal/visualize-modal.component.ts
@@ -20,20 +20,12 @@ export class VisualizeModalComponent implements OnInit {
   ngOnInit(): void {
     console.log(this.nodeName)
     //Axios.get(`http://localhost:5000/data/${this.nodeName}`)
-<<<<<<< HEAD
-    const btn = document.getElementsByClassName("btnDetail");
-    btn.addEventListener("click",(e)=>{
-      Axios.get(`http://localhost:5000/data/${this.nodeName}`).then((resp)=>{
-        console.log(resp)
-        this.dataArr_recv = resp.data
-=======
     Axios.get(`http://localhost:5000/fakedata/${this.nodeName}`).then((resp)=>{
         console.log(resp)
         this.dataArr_recv = resp.data
         console.log(this.dataArr_recv)
         //this.dataArr_recv = this.dataArr_recv.filter((data)=>{return data.nodeName === this.nodeName})
         this.dataArr_recv = this.dataArr_recv.slice(0,10)
->>>>>>> a734d686459f1a07e8d743b3d5c38b207149b0be
       })
     console.log(this.dataArr_recv)
   }
